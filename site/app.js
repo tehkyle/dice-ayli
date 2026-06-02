6577c916e4d0686f26e7402cf87f04ce5735a0e8
Kyle Levien <tehkyle@gmail.com>
2026-06-01 23:28:47 -0700

    move qlab zip download out of github and into dropbox

--- site/app.js
+++ site/app.js
@@ -1,4 +1,4 @@
 const BASE = 'https://github.com/tehkyle/dice-ayli/releases/latest/download';
 
 document.getElementById('btn-download').href = `${BASE}/Dacha-Dice-AYLI-mac.dmg`;
-document.getElementById('btn-qlab').href      = `${BASE}/qlab-show-files.zip`;
+document.getElementById('btn-qlab').href      = '__QLAB_DOWNLOAD_URL__';
